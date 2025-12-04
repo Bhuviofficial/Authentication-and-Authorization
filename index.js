2cdf90c22f98e4c8258685bdf2249b863672c02e
Bhuviofficial <bhuvi1928.g@gmail.com>
2025-12-04 20:06:54 +0530

    done

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const app = express();
 
 app.use(express.json()); 
 
+
+app.get("/", (req, res) => {
+    res.send("Server is running successfully!");
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
